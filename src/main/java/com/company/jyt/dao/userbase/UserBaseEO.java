3f609b0b23d82a37266c1143cda7e169af770743
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2018-01-31 14:36:12 +0800

    更新代码

---
+++ src/main/java/com/company/jyt/dao/userbase/UserBaseEO.java
@@ -0,0 +1,6 @@
+package com.company.jyt.dao.userbase;
+
+import com.company.jyt.support.db.BaseEO;
+
+public class UserBaseEO extends BaseEO {
+}
---
+++ src/main/java/com/company/jyt/support/db/BaseEO.java
@@ -0,0 +1,6 @@
+package com.company.jyt.support.db;
+
+import java.io.Serializable;
+
+public class BaseEO implements Serializable {
+}
