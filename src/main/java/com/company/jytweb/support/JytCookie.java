a88481fd4903c3386567fae190cd02b5bf15f68c
wangzhenjun <wangzhenjun@okay.cn>
2018-02-02 18:32:19 +0800

    更新代码

--- src/main/java/com/company/jytweb/support/JytCookie.java
+++ src/main/java/com/company/jytweb/support/JytCookie.java
@@ -12,9 +12,9 @@ import java.util.Map;
  */
 public class JytCookie {
 
-    /*  */
+    /* Key Name Ucp */
     private static final String KEY_NAME_UCP = "_ucp";
-    /*  */
+    /* Key Name Attention */
     private static final String KEY_NAME_ATTENTION = "_attention";
 
     private Map<String, String> cookies;
@@ -42,5 +42,12 @@ public class JytCookie {
      */
     public String getAttention() {
         return cookies.get(KEY_NAME_ATTENTION);
+    }
+
+    public static void main(String[] args) {
+        String str = "__jsluid=273f0a1e12bf37723cc806c612d54a4b; gr_user_id=91cab4c5-d7ce-442e-a876-0da46dea73c1; _ucp=lw7OO71jLwQVfTfMD4QoKXImsz1sYudnN-uu--3_t0VVunIxXrjwwB6vF37o_yvZneAlvw..; _attention=1; _lgd=1; m-uni-ticket=k5tM6QDIQ7BBD84TJAG5Qfvq_RpcN0K9qp1h9qffuQyZhBOPIvU_70bLd3hLMXtwMA1J3g..; gr_session_id_baad359c5e3b60a1=51002463-7486-4d1f-bd4b-f7b1d8435cad";
+        JytCookie cookie =  new JytCookie(str);
+        System.out.println(cookie.getUcp());
+        System.out.println(cookie.getAttention());
     }
 }
