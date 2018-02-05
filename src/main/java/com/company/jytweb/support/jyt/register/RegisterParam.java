811c952aef3237246a4945d02b289ff2c5ea655e
wangzhenjun <wangzhenjun@okay.cn>
2018-02-05 17:00:18 +0800

    更新代码

--- src/main/java/com/company/jytweb/support/jyt/register/RegisterParam.java
+++ src/main/java/com/company/jytweb/support/jyt/register/RegisterParam.java
@@ -9,6 +9,15 @@ import java.io.Serializable;
  */
 public class RegisterParam implements Serializable {
 
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /*       就诊人          */
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /* 医保卡号 */
+    private String cardNo;
+
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /*      就诊科室         */
+    /* >>>>>>>>>>>>>>>>>>>> */
     /* 医院编号 */
     private String hosCode;
     /* 一级科室编码 */
@@ -20,21 +29,31 @@ public class RegisterParam implements Serializable {
     /* 二级科室编号 */
     private Long secondDeptId;
 
-    /* 号源编号 */
-    private String productId;
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /*      就诊时段         */
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /* 就诊时间 */
+    private String treatmentDay;
+    /* 就诊时段 */
+    private String productTimeType;
+
+    /* >>>>>>>>>>>>>>>>>>>> */
+    /*       医生信息        */
+    /* >>>>>>>>>>>>>>>>>>>> */
     /* 医生职称 */
     private String productType;
-    /* 看病时段 */
-    private String productTimeType;
     /* 医生编号 */
     private Long doctorId;
-    /* 看病时间 */
-    private String treatmentDay;
+
+    /* 号源编号 */
+    private String productId;
     /*  */
     private String orderProductType;
     /*  */
     private String orderFrom;
+    /*  */
+    private String price;
+    /*  */
+    private String regHour;
 
-    /* 医保卡号 */
-    private String cardNo;
 }
