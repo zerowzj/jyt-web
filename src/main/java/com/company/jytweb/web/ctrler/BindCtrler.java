c6cacadc16badbc3cd633f342b38e15299377916
wangzhenjun <wangzhenjun@okay.cn>
2018-02-05 18:35:48 +0800

    更新代码

---
+++ src/main/java/com/company/jytweb/web/ctrler/BindCtrler.java
@@ -0,0 +1,14 @@
+package com.company.jytweb.web.ctrler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BindCtrler {
+
+    @RequestMapping("/bind")
+    public void bind() {
+
+
+    }
+}
