01d70c9af82fd8849be8bf6a63c139ef894fc043
wangzhenjun <wangzhenjun@okay.cn>
2018-02-05 19:28:09 +0800

    更新代码

--- src/main/java/com/company/jytweb/support/JytCookieCache.java
+++ src/main/java/com/company/jytweb/support/JytCookieCache.java
@@ -3,15 +3,23 @@ package com.company.jytweb.support;
 import com.company.jytweb.dao.userjytinfo.UserJytInfoDao;
 import com.company.jytweb.dao.userjytinfo.UserJytInfoEO;
 import com.company.jytweb.support.jyt.JytCookie;
-import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * JytCookie缓存
+ *
+ * @author wangzhj
+ */
 public class JytCookieCache {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JytCookieCache.class);
@@ -19,12 +27,19 @@ public class JytCookieCache {
     @Autowired
     private UserJytInfoDao userJytInfoDao;
 
+    /*  */
     public static JytCookieCache JYT_COOKIE_CACHE;
-
-    private static Cache<Long, JytCookie> CACHE = Caffeine.newBuilder()
+    /*  */
+    private static LoadingCache<Long, JytCookie> CACHE = Caffeine.newBuilder()
             .maximumSize(10000)
-            .expireAfterWrite(10, TimeUnit.MINUTES)
-            .build();
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .build(new CacheLoader<Long, JytCookie>() {
+                @CheckForNull
+                @Override
+                public JytCookie load(@Nonnull Long key) throws Exception {
+                    return null;
+                }
+            });
 
     @PostConstruct
     public void init() {
@@ -40,6 +55,7 @@ public class JytCookieCache {
     public static JytCookie get(Long ubId) {
         JytCookie cookie = CACHE.getIfPresent(ubId);
         if (cookie == null) {
+            LOGGER.info("===> get jyt cookie of [ub={}] from db", ubId);
             UserJytInfoEO ujiEO = JYT_COOKIE_CACHE.userJytInfoDao.getByUbId(ubId);
             if (ujiEO != null) {
                 cookie = new JytCookie(ujiEO.getUjiJytCookie());
