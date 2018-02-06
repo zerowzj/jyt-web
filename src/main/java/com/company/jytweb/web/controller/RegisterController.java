cefbf8a8568fd10d889b9974b8acfd0536e4639a
wangzhenjun <wangzhenjun@okay.cn>
2018-02-06 11:08:48 +0800

    更新代码

--- src/main/java/com/company/jytweb/web/controller/RegisterController.java
+++ src/main/java/com/company/jytweb/web/controller/RegisterController.java
@@ -2,8 +2,16 @@ package com.company.jytweb.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
+
+    @RequestMapping("/toCheckin")
+    public ModelAndView toBind(HttpServletRequest request) {
+        return new ModelAndView("/jyt/bind");
+    }
 }
